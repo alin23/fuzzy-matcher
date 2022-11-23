92d324f4e981c3258c42ae84a90181b694b8a2dc
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-11-23 15:49:26 +0200

    Update fuzzy matcher

--- build.sh
+++ build.sh
@@ -12,9 +12,17 @@ export MACOSX_DEPLOYMENT_TARGET=11.0
 # Build the project for the desired platforms:
 cargo build --target x86_64-apple-darwin --release
 cargo build --target aarch64-apple-darwin --release
-mkdir -p ./target/universal-macos/debug
+mkdir -p ./target/universal-macos/$1
 
 lipo \
-    ./target/aarch64-apple-darwin/debug/libfuzzy_matcher.a \
-    ./target/x86_64-apple-darwin/debug/libfuzzy_matcher.a -create -output \
-    ./target/universal-macos/debug/libfuzzy_matcher.a
+    ./target/aarch64-apple-darwin/$1/libfuzzy_matcher.a \
+    ./target/x86_64-apple-darwin/$1/libfuzzy_matcher.a -create -output \
+    ./target/universal-macos/$1/libfuzzy_matcher.a
+
+swift-bridge-cli create-package \
+--bridges-dir ./generated \
+--out-dir FuzzyMatcher \
+--macos target/universal-macos/$1/libfuzzy_matcher.a \
+--name FuzzyMatcher
+
+cp target/universal-macos/$1/libfuzzy_matcher.a FuzzyMatcher/RustXcframework.xcframework/macos-arm64_x86_64/libfuzzy_matcher.a
